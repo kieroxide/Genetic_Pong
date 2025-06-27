5a31e0dfcb322ca1f451f6305fc5d2182c8b0a92
Kieroxide <kieranbprivate@gmail.com>
2025-06-27 13:14:04 +0100

    Refactored Main to be cleaner. Added Globals for easy tweaking

--- main.py
+++ main.py
@@ -4,34 +4,60 @@ import numpy as np
 import random
 import multiprocessing
 
+
+MUTATION_RATE = 0.1
+NEURON_SIZE = 1
+SCORE_WEIGHT = 20
+MAX_FRAMES = 500
+
 def main():
     MAX_GENERATIONS = 10
     POP_SIZE = 1000
     PARENT_SIZE = round(POP_SIZE * 0.1)
-    population = [NeuralNet(6, 16, 1) for _ in range(POP_SIZE)]
-    best = population[0]
-    for gen in range(MAX_GENERATIONS):
-        print(f"Generation: {gen}")
-        with multiprocessing.Pool() as pool:
-            population = pool.map(evaluate_net, population)
-        selectBest(population)
-        parents = population[:PARENT_SIZE]
-        children = []
-        while len(children) < POP_SIZE - len(parents):
-            p1, p2 = random.sample(parents, 2)
-            child = crossover(p1, p2)
-            mutate(child)
-            children.append(child)
 
-        population = parents + children
-        best = population[0]
-    pong = Pong(drawn=True, net=best, maxFrames=float('inf'))
+    #Generates NeuralNets for to be each member of the population
+    #NeuralNet(input_size, Hidden_Size, Output_Size)
+    population = [NeuralNet(6, NEURON_SIZE, 1) for _ in range(POP_SIZE)]
+
+    best_performer = simulate_generations(MAX_GENERATIONS, POP_SIZE, PARENT_SIZE, population)
+    
+    #Display best performer game
+    pong = Pong(drawn=True, net=best_performer, maxFrames=float('inf'))
     pong.start()
 
+def simulate_generations(MAX_GENERATIONS, POP_SIZE, PARENT_SIZE, population):
+    for gen in range(MAX_GENERATIONS):
+        print(f"Generation: {gen}")
+        #Simulates generation and returns top performers
+        parents = rank_generation(population, PARENT_SIZE)
+        best_performer = parents[0]
+        #Repopulates with parents, mutates to avoid homogenity
+        children = repopulate(POP_SIZE, parents)
+        #Joins them both to create the next generation
+        population = parents + children
+        #Best performer to watch after all generations done
+    return best_performer
+
+def repopulate(POP_SIZE, parents):
+    children = []
+    while len(children) < POP_SIZE - len(parents):
+        p1, p2 = random.sample(parents, 2)
+        child = crossover(p1, p2)
+        mutate(child)
+        children.append(child)
+    return children
+
+def rank_generation(population, PARENT_SIZE):
+    with multiprocessing.Pool() as pool:
+        population = pool.map(evaluate_net, population)
+    selectBest(population)
+    parents = population[:PARENT_SIZE]
+    return parents
+
 def evaluate_net(net):
-    pong = Pong(drawn=True, net=net, maxFrames=500)
+    pong = Pong(drawn=False, net=net, maxFrames=MAX_FRAMES)
     frames, score = pong.start()
-    net.fitness = score*20 + frames
+    net.fitness = score*SCORE_WEIGHT + frames
     return net
 
 def crossover(parent1, parent2):
@@ -43,7 +69,7 @@ def crossover(parent1, parent2):
 
     return child
 
-def mutate(net, mutation_rate=0.1):
+def mutate(net, mutation_rate=MUTATION_RATE):
     net.w1 += np.random.randn(*net.w1.shape) * mutation_rate
     net.w2 += np.random.randn(*net.w2.shape) * mutation_rate
 
